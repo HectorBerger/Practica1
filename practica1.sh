3b9dcf9ea78021381762ea300e980736082dbc7a
Héctor Berger <1751751@uab.cat>
2024-09-30 09:41:56 +0200

    Creació supervivents_csv i Primer pas

--- practica1.sh
+++ practica1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "Hola"
+#Primer pas, eliminar el thumbnail_link i la description perquè no aporten informació important. Són els identificadors 12 i 16 respectivament.
+cut –d “,” -f 12 supervivents.csv 
