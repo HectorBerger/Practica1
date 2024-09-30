1eee41995f1085f347cd593bdc62e6fe3387abc5
Gonzalo <1699947@uab.cat>
2024-09-30 10:06:40 +0200

    PrimerDia_carrega3_cut

--- practica1.sh
+++ practica1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 #Primer pas, eliminar el thumbnail_link i la description perquè no aporten informació important. Són els identificadors 12 i 16 respectivament.
-cut –d “,” -f 12 supervivents.csv 
+cut --complement -d "," -f 12,16 -n supervivents.csv > proba.txt 
