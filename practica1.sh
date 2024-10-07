1f24cbaa095b4f05c80197e6956cbfabd9c4bf16
Gonzalo <1699947@uab.cat>
2024-10-07 10:26:08 +0200

    carrega_final_Dia2

--- practica1.sh
+++ practica1.sh
@@ -1,4 +1,11 @@
- #!/bin/bash
+#!/bin/bash
 
 #Primer pas, eliminar el thumbnail_link i la description perquè no aporten informació important. Són els identificadors 12 i 16 respectivament.
-cut --complement -d "," -f 12,16 -n supervivents.csv > proba.txt 
+cut --complement -d"," -f 12,16  supervivents.csv > primera_versio.csv
+
+#
+awk -F',' '{ if($14=="False") print$0 }' primera_versio.csv > segona_versio.csv
+linies_original=$(wc -l < primera_versio.csv)
+linies_resta=$(wc -l < segona_versio.csv)
+
+echo "S'han eliminat tantes files: $(($linies_original-$linies_resta))"
