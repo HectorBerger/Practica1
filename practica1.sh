b01b10cb8996995b3b7a763f808ffb6d5289091b
Gonzalo <1699947@uab.cat>
2024-10-14 20:25:07 +0200

    Adhesio de columnes like i dislike

--- practica1.sh
+++ practica1.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 #Primer pas, eliminar el thumbnail_link i la description perquè no aporten informació important. Són els identificadors 12 i 16 respectivament.
@@ -10,4 +11,20 @@ linies_resta=$(wc -l < segona_versio.csv)
 
 echo "S'han eliminat tantes files: $(($linies_original-$linies_resta))"
 
-awk -F',' '{if(NR==1) print$0",Ranking_Views"; if ($8<1000000) print$0",Bo";else if ($8<10000000) print$0",Excel·lent"; else print$0",Estrella"}' segona_versio.csv > tercera_versio.csv
+awk -F',' '{if(NR==1) print$0",Ranking_Views";else if ($8<1000000) print$0",Bo";else if ($8<10000000) print$0",Excel·lent"; else print$0",Estrella"}' segona_versio.csv > tercera_versio.csv
+
+for ((i=1;i<=$linies_resta;i++));do 
+	read line
+	if [ $i -eq 1 ];then
+		echo "$line,Rlikes,Rdislikes" > sortida.csv
+	else
+		V=$(echo "$line" | cut -d',' -f 8)
+                L=$(echo "$line" | cut -d',' -f 9)
+                DL=$(echo "$line" | cut -d',' -f 10)
+                echo "$line,$((L*100/$V)),$(($DL*100/$V))" >> sortida.csv
+
+	fi
+
+
+
+done < tercera_versio.csv
